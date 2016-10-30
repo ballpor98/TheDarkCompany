578f2b87904c5631e493f61b9bf7aba305ef819e
InoriSan <ballpor98@gmail.com>
2016-10-30 23:24:03 +0700

    add template

--- gamingSpot/gamingSpot/urls.py
+++ gamingSpot/gamingSpot/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    url(r'^', include('shop.urls')),
+    url(r'^shop/', include('shop.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
