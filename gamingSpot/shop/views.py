1db4922aaa948d3533040cfdb4100f026b23a5ca
InoriSan <ballpor98@gmail.com>
2016-11-15 19:41:42 +0700

    update views.py

--- gamingSpot/shop/views.py
+++ gamingSpot/shop/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views import generic
+from django.views import generic,View
 from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
 
 from .models import *
 from .forms import *
@@ -20,10 +21,29 @@ class DetailView(generic.DetailView):
     model = Product
     template_name = 'shop/detail.html'
 
-class RegisterView(FormView):
-    template_name = 'shop/regis.html'
-    form_class = RegisterForm
-    success_url = '/shop/'
+class ContactView(TemplateView):
+    model = Product
+    template_name = "shop/contact-us-page.html"
 
-    def form_valid(self, form):
-        return super(RegisterView, self).form_valid(form)
+class RegisterView(View):
+    template_name = 'shop/regis.html'
+    # success_url = '/shop/'
+
+    #def form_valid(self, form):
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+    def post(self, request):
+        forms = {}
+        for key in request.POST.keys():
+            forms[key] = request.POST.get(key, '')
+        newuser = MyUser(is_superuser=0,
+                    username=forms['display_name'],
+                    first_name=forms['first_name'],
+                    last_name=forms['last_name'],
+                    email=forms['email'])
+        newuser.set_password(forms['password'])
+        newuser.save()
+        #return redirect('/shop')
+
+        return render(request, self.template_name, forms)
