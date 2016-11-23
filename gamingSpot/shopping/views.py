b2a655e13845fe1d5e234020f01decadc7e98099
InoriSan <ballpor98@gmail.com>
2016-11-23 13:12:38 +0700

    add cart

---
+++ gamingSpot/shopping/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from carton.cart import Cart
+from shop.models import Product
+
+
+def add(request):
+    cart = Cart(request.session)
+    product = Product.objects.get(id=request.GET.get('id'))
+    cart.add(product, price=product.price)
+    return HttpResponse("Added")
+
+
+def remove(request):
+    cart = Cart(request.session)
+    product = Product.objects.get(id=request.GET.get('id'))
+    cart.remove(product)
+    return HttpResponse("Removed")
+
+
+def show(request):
+    #cart = Cart(request.session)
+    return render(request, 'shopping/show-cart.html')
