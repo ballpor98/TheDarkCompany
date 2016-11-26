cf7b4579bb45241a910a93273254d3fbf97b2949
InoriSan <ballpor98@gmail.com>
2016-11-26 20:21:50 +0700

    order update

--- gamingSpot/shop/urls.py
+++ gamingSpot/shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^order/$',views.OrderView.as_view(), name='order'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail')
 ]
