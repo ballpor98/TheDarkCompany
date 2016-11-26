8677167bb06f34d44b8f65fe5fe9cab369f9a7a5
Ubuntu 16.04 <nitisit.j@ku.th>
2016-11-26 06:29:58 -0800

    Checkoutview in views

--- gamingSpot/shopping/views.py
+++ gamingSpot/shopping/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+from django.views.generic import TemplateView
 
 from carton.cart import Cart
 from shop.models import *
@@ -23,3 +24,6 @@ def show(request):
     #cart = Cart(request.session)
     return render(request, 'shopping/show-cart.html')
 
+class CheckoutView(TemplateView):
+    template_name = "shopping/checkout.html"
+    
