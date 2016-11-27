3788586be38c113055eda871108d638fa8c145bc
InoriSan <ballpor98@gmail.com>
2016-11-28 05:51:33 +0700

    add some action

--- gamingSpot/shop/admin.py
+++ gamingSpot/shop/admin.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render,redirect
+
 from .models import *
 # Register your models here.
 
@@ -20,6 +24,12 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('member_id','status','date','total','product_list')
     list_filter = ['member_id','status','date','total']
     list_per_page = 50
+    actions = ['test']
+
+    def test(modeladmin, request, queryset):
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        ct = ContentType.objects.get_for_model(queryset.model)
+        return redirect("/shop/?id="+str(selected[0]))
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username','first_name','last_name','email','tels')
@@ -27,7 +37,5 @@ class UserAdmin(admin.ModelAdmin):
     list_per_page = 50
 
 admin.site.register(Product,ProductAdmin)
-'''admin.site.register(Member)'''
 admin.site.register(Order,OrderAdmin)
 admin.site.register(MyUser,UserAdmin)
-#admin.site.register(Description)
