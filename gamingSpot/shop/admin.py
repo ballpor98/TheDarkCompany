164b00533fcc00664eb0be2fd2939ff9a23557c1
Ubuntu 16.04 <nitisit.j@ku.th>
2016-11-27 13:31:06 -0800

    Add fukcing admin

--- gamingSpot/shop/admin.py
+++ gamingSpot/shop/admin.py
@@ -10,16 +10,24 @@ from .models import *
     extra = 1'''
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','brand','price','lates_update')
-    list_filter = ['brand','lates_update']
+    list_display = ('name','categories','brand','price','lates_update')
+    list_filter = ['categories','brand','lates_update']
     #inlines = [ImageInline]
+    list_per_page = 50
+    search_fields = ['name','categories','brand',]
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('member_id','status','date','total','product_list')
     list_filter = ['member_id','status','date','total']
+    list_per_page = 50
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','first_name','last_name','email','tels')
+    search_fields = ['username','first_name','last_name','email','tels']
+    list_per_page = 50
 
 admin.site.register(Product,ProductAdmin)
 '''admin.site.register(Member)'''
 admin.site.register(Order,OrderAdmin)
-admin.site.register(MyUser)
+admin.site.register(MyUser,UserAdmin)
 #admin.site.register(Description)
